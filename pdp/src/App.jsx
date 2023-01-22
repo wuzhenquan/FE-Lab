ec7a2bb643e181b32f72b5739b9aea626eb979b5
巫镇泉 <zhenquan.me@gmail.com>
2023-01-22 17:33:34 +0800

    feat: Sharing Functions

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./index.scss";
 
 import Header from "home/Header";
 import Footer from 'home/Footer';
+import PDPContent from "./PDPcontent";
 
 const App = () => (
-	<div className="text-3xl mx-auto max-w-6xl">
-		<Header />
-		<div className="my-10">PDP Page Content</div>
-    <Footer />
-	</div>
+	<Router>
+		<div className="text-3xl mx-auto max-w-6xl">
+			<Header />
+			<div className="my-10">
+				<Routes>
+					<Route path="/product/:id" element={<PDPContent />} />
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	</Router>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
