0887fb1b52ff7d5f4b7f4379c950f0bbddedfdab
巫镇泉 <zhenquan.me@gmail.com>
2023-01-29 18:19:20 +0800

    feat: Sharing the JWT

---
+++ cart/src/CartContent.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./Login";
+import { login, jwt } from "./cart";
+
+export default function CartContent() {
+	const [token, setToken] = useState();
+
+
+  useEffect(() => {
+    return jwt.subscribe(val => setToken(val ?? ""));
+  }, []);
+
+	return (
+		<div>
+			<div></div>JWT: {token}
+			<Login />
+		</div>
+	);
+}
---
+++ cart/src/cart.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { BehaviorSubject } from "rxjs";
+
+const API_SERVER = "http://localhost:8080";
+
+export const jwt = new BehaviorSubject(null);
+
+export const login = (username, password) => {
+	fetch(`${API_SERVER}/auth/login`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username,
+			password,
+		}),
+	})
+		.then(res => res.json())
+		.then(data => {
+      jwt.next(data.access_token);
+      return data.access_token;
+    });
+};
+
+export function useLoggedIn() {
+	const [loggedIn, setLoggedIn] = useState(!!jwt.value);
+	useEffect(() => {
+		setLoggedIn(!!jwt.value);
+		return jwt.subscribe(c => {
+			setLoggedIn(!!jwt.value);
+		});
+	}, [])
+	
+	return loggedIn;
+}
