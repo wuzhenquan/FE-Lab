5576eae6234e0312e0ab6500845e86d9a054dec6
巫镇泉 <zhenquan.me@gmail.com>
2023-01-30 18:00:36 +0800

    commit message

---
+++ pdp/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "remixicon/fonts/remixicon.css";
+import "./index.scss";
+
+import Header from "home/Header";
+import Footer from 'home/Footer';
+import PDPContent from "./PDPContent";
+
+const App = () => (
+	<Router>
+		<div className="text-3xl mx-auto max-w-6xl">
+			<Header />
+			<div className="my-10">
+				<Routes>
+					<Route path="/product/:id" element={<PDPContent />} />
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	</Router>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
